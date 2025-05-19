1e10d0e17219a9b9f2eecfc43684c80390d26284
Rodrigo Falcão <contato.rodrigofalcao@gmail.com>
2025-05-19 19:05:11 -0300

    Feat: Create Theme

--- src/main.tsx
+++ src/main.tsx
@@ -1,9 +1,14 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
+import { ThemeProvider } from 'styled-components'
 import App from './App.tsx'
+import { createRoot } from 'react-dom/client'
+import { StrictMode } from 'react'
+import { GlobalStyle, lightTheme } from './styles'
 
 createRoot(document.getElementById('root')!).render(
-  <StrictMode>
-    <App />
-  </StrictMode>
+    <StrictMode>
+        <ThemeProvider theme={lightTheme}>
+            <GlobalStyle />
+            <App />
+        </ThemeProvider>
+    </StrictMode>
 )
