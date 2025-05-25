41f570397353beeca0503c3693765d458dbe7cf5
Rodrigo Falcão <contato.rodrigofalcao@gmail.com>
2025-05-25 15:08:40 -0300

    Feat: Login page requests

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,33 @@
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
+import {
+    BrowserRouter as Router,
+    Routes,
+    Route,
+    Navigate,
+    Outlet,
+} from 'react-router-dom'
 import { Login, Registration, Home, Leads, Profile } from './pages'
+import Cookies from 'js-cookie'
 
 function App() {
+    const ProtectedRoute = () => {
+        const checkAuthCookie = Cookies.get('Authorization')
+        if (!checkAuthCookie) {
+            alert('Autenticação necessária!')
+            return <Navigate to="/" replace />
+        }
+        return <Outlet />
+    }
+
     return (
         <Router>
             <Routes>
                 <Route path="/" element={<Login />} />
                 <Route path="/cadastro" element={<Registration />} />
-                <Route path="/home" element={<Home />} />
-                <Route path="/leads" element={<Leads />} />
-                <Route path="/perfil" element={<Profile />} />
+                <Route element={<ProtectedRoute />}>
+                    <Route path="/home" element={<Home />} />
+                    <Route path="/leads" element={<Leads />} />
+                    <Route path="/perfil" element={<Profile />} />
+                </Route>
             </Routes>
         </Router>
     )
--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -1,3 +1,10 @@
+import { useEffect } from 'react'
+import type { ChangeEvent } from 'react'
+import { useNavigate } from 'react-router-dom'
+import { jwtDecode } from 'jwt-decode'
+import Cookies from 'js-cookie'
+
+// COMPONENTS
 import { Box, Container, Grid } from '@mui/material'
 import {
     BannerImage,
@@ -6,9 +13,63 @@ import {
     StyledP,
     Logo,
 } from '@/components'
-import { pxToRem } from '@/utils'
+
+// HOOKS
+import { useFormValidation, usePost } from '@/hooks'
+
+// UTILS
+import { jwtExpirationDateConverter, pxToRem } from '@/utils'
+
+// TYPES
+import type {
+    MessageProps,
+    LoginData,
+    LoginPostData,
+    DecodedJWT,
+} from '@/types'
 
 function Login() {
+    const navigate = useNavigate()
+    const inputs = [
+        { type: 'email', placeholder: 'Email' },
+        { type: 'password', placeholder: 'Senha' },
+    ]
+    const { data, loading, error, postData } = usePost<
+        LoginData,
+        LoginPostData
+    >('login')
+    const { formValues, formValid, handleChange } = useFormValidation(inputs)
+    const handleMessage = (): MessageProps => {
+        if (!error) return { msg: '', type: 'success' }
+        switch (error) {
+            case 401:
+                return { msg: 'Email e/ou senha inválidos', type: 'error' }
+            default:
+                console.log('error:', error)
+                return {
+                    msg: 'Não foi possível realizar a operação. Entre em contato com o suporte.',
+                    type: 'error',
+                }
+        }
+    }
+    const handleSubmit = async (e: React.FormEvent) => {
+        e.preventDefault()
+        await postData({
+            email: String(formValues[0]),
+            password: String(formValues[1]),
+        })
+    }
+    useEffect(() => {
+        if (data?.jwt_token) {
+            const decoded: DecodedJWT = jwtDecode(data?.jwt_token)
+            Cookies.set('Authorization', data?.jwt_token, {
+                expires: jwtExpirationDateConverter(decoded.exp),
+                secure: true,
+            })
+        }
+        if (Cookies.get('Authorization')) navigate('/home')
+    }, [data, navigate])
+
     return (
         <>
             <Box>
@@ -34,26 +95,30 @@ function Login() {
                                 </StyledP>
                             </Box>
                             <FormComponent
-                                inputs={[
-                                    {
-                                        type: 'email',
-                                        placeholder: 'Email',
-                                        disabled: true,
-                                    },
-                                    { type: 'password', placeholder: 'Senha' },
-                                ]}
+                                inputs={inputs.map((input, index) => ({
+                                    type: input.type,
+                                    placeholder: input.placeholder,
+                                    value: formValues[index] || '',
+                                    onChange: (
+                                        e: ChangeEvent<HTMLInputElement>
+                                    ) =>
+                                        handleChange(
+                                            index,
+                                            (e.target as HTMLInputElement).value
+                                        ),
+                                }))}
                                 buttons={[
                                     {
                                         className: 'primary',
+                                        disabled: !formValid || loading,
                                         type: 'submit',
-                                        children: 'Login',
-                                        disabled: true,
+                                        onClick: handleSubmit,
+                                        children: loading
+                                            ? 'Aguarde...'
+                                            : 'Login',
                                     },
                                 ]}
-                                message={{
-                                    msg: 'Sucesso!!',
-                                    type: 'success',
-                                }}
+                                message={handleMessage()}
                             />
                         </Container>
                     </Grid>
