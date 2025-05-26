60fd97518526de8868caa847cae377449be909f2
Rodrigo Falcão <contato.rodrigofalcao@gmail.com>
2025-05-26 20:05:22 -0300

    Feat: Profile Request

--- src/hooks/useAxios.ts
+++ src/hooks/useAxios.ts
@@ -44,6 +44,7 @@ export const usePost = <T, P>(endpoint: string) => {
     return { data, loading, error, postData }
 }
 
+// REQUISIÇÃO GET
 export const useGet = <T>(endpoint: string, config?: AxiosRequestConfig) => {
     const [data, setData] = useState<T | null>(null)
     const [loading, setLoading] = useState(false)
@@ -80,4 +81,72 @@ export const useGet = <T>(endpoint: string, config?: AxiosRequestConfig) => {
     }, [])
 
     return { data, loading, error, getData }
+}
+
+// REQUISIÇÃO PUT
+export const usePut = <T>(endpoint: string) => {
+    const [data, setData] = useState<T | null>(null)
+    const [loading, setLoading] = useState(false)
+    const [error, setError] = useState<number | null>(null)
+
+    const putData = async (putData: T, config?: AxiosRequestConfig) => {
+        setData(null)
+        setLoading(true)
+        setError(null)
+
+        try {
+            const response = await axiosInstance({
+                url: endpoint,
+                method: 'PUT',
+                data: putData,
+                headers: {
+                    Authorization: `Bearer ${Cookies.get('Authorization')}`,
+                    'Content-Type': 'application/json',
+                    ...config?.headers,
+                },
+                ...config,
+            })
+            setData(response.data)
+        } catch (e: any) {
+            if (e.response && e.response.status) {
+                setError(e.response.status)
+            } else {
+                setError(500)
+            }
+        } finally {
+            setLoading(false)
+        }
+    }
+
+    return { data, loading, error, putData }
+}
+
+// REQUISIÇÃO DELETE
+export const useDelete = <T>(endpoint: string) => {
+    const [data, setData] = useState<T | null>(null)
+    const [loading, setLoading] = useState(false)
+
+    const deleteData = async (config?: AxiosRequestConfig) => {
+        setData(null)
+        setLoading(true)
+
+        try {
+            const response = await axiosInstance({
+                url: endpoint,
+                method: 'DELETE',
+                headers: {
+                    Authorization: `Bearer ${Cookies.get('Authorization')}`,
+                    ...config?.headers,
+                },
+                ...config,
+            })
+            setData(response.data)
+        } catch (e: any) {
+            throw e.response?.status
+        } finally {
+            setLoading(false)
+        }
+    }
+
+    return { data, loading, deleteData }
 }
