c6462526dc7d7cfed3499e5442e199a18a22457f
Rodrigo Falcão <contato.rodrigofalcao@gmail.com>
2025-05-30 21:23:59 -0300

    Feat: Create profile flow

--- src/main.tsx
+++ src/main.tsx
@@ -3,12 +3,16 @@ import { createRoot } from 'react-dom/client'
 import { GlobalStyle } from './styles'
 import { StrictMode } from 'react'
 import App from './App.tsx'
+import { Provider } from 'react-redux'
+import store from './redux/index.ts'
 
 createRoot(document.getElementById('root')!).render(
     <StrictMode>
-        <AppThemeProvider>
-            <GlobalStyle />
-            <App />
-        </AppThemeProvider>
+        <Provider store={store}>
+            <AppThemeProvider>
+                <GlobalStyle />
+                <App />
+            </AppThemeProvider>
+        </Provider>
     </StrictMode>
 )
--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -28,8 +28,15 @@ import type {
     DecodedJWT,
 } from '@/types'
 
+// REDUX
+import { useSelector } from 'react-redux'
+import type { RootState } from '@/redux'
+
 function Login() {
     const navigate = useNavigate()
+    const { email, message } = useSelector(
+        (state: RootState) => state.createProfile
+    )
     const inputs = [
         { type: 'email', placeholder: 'Email' },
         { type: 'password', placeholder: 'Senha' },
@@ -40,7 +47,7 @@ function Login() {
     >('login')
     const { formValues, formValid, handleChange } = useFormValidation(inputs)
     const handleMessage = (): MessageProps => {
-        if (!error) return { msg: '', type: 'success' }
+        if (!error) return { msg: message ?? '', type: 'success' }
         switch (error) {
             case 401:
                 return { msg: 'Email e/ou senha inválidos', type: 'error' }
@@ -70,6 +77,11 @@ function Login() {
         if (Cookies.get('Authorization')) navigate('/home')
     }, [data, navigate])
 
+    useEffect(() => {
+        if (email) {
+            handleChange(0, email)
+        }
+    }, [email])
     return (
         <>
             <Box>
